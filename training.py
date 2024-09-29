680c04d11d9168db87823e4eeb8a98f5a25f52e4
confidenceaffang <coaffa01@wsc.edu>
2024-09-29 02:04:25 +0000

    model added and saved using .keras extension

--- training.py
+++ training.py
@@ -10,7 +10,7 @@ nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
-
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
@@ -38,3 +38,36 @@ classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pk1', 'wb'))
 pickle.dump(classes, open('classes.pk1', 'wb'))
+
+training =[]
+
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag =[]
+    word_pattern = document[0]
+    word_pattern = [lemmatizer.lemmatize(word.lower()) for word in word_pattern]
+    for word in words:
+        bag.append(1) if word in word_pattern else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training, dtype = object)
+trainx = list(training[:, 0])
+trainy = list(training[:, 1])
+
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(trainx[0]),), activation='relu'))  # Fixed input_shape
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(trainy[0]), activation='softmax'))
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(np.array(trainx), np.array(trainy), epochs= 200, batch_size= 5, verbose = 1)
+
+model.save('chatbot_model.keras')
+print('Done')
